b8dfbb1b1a1df22587729877ebde64c5eab44407
JaideepSinghChouhan <jd8769023@gmail.com>
2025-04-22 19:58:06 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import chef from "../assets/chef-claude-icon.png"
+export default function Header(){
+    return(
+        <>
+        <header>
+            <img src={chef} alt="" />
+            <h1>Chef Claude</h1>
+        </header>
+        </>
+    )
+}
